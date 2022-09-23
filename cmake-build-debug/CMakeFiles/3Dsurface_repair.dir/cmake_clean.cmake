dbf29baa912bd7011850a900c961e48ede3d6367
YitongXia <xiayitong0630@gmail.com>
2022-09-23 23:21:24 +0200

    initial commit

---
+++ cmake-build-debug/CMakeFiles/3Dsurface_repair.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "3Dsurface_repair"
+  "3Dsurface_repair.pdb"
+  "CMakeFiles/3Dsurface_repair.dir/main.cpp.o"
+  "CMakeFiles/3Dsurface_repair.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3Dsurface_repair.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/3Dsurface_repair.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 3Dsurface_repair.
+# This may be replaced when dependencies are built.
